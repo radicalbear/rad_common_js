9ba6b11c752ca5df92390996674381f0c89fdf6f
Eric Bundy <elbzero@gmail.com>
2022-03-10 14:59:50 -0500

    add some style overrides

--- src/stylesheets.js
+++ src/stylesheets.js
@@ -4,3 +4,4 @@ import('./css/rad_common/rad_common.css.scss');
 import('./css/bootstrap_and_overrides.scss');
 import('./css/bootstrap_datetimepicker/dates.scss')
 import('./css/deviseAuthy.scss');
+import('./css/rad_common/jquery-ui-overrides.scss')
