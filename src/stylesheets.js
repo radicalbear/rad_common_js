6f81dd6b1cee828e3062475c2c02782c33ee6016
Eric Bundy <elbzero@gmail.com>
2022-03-10 08:42:49 -0500

    move some style sheets to rad_common_js

--- src/stylesheets.js
+++ src/stylesheets.js
@@ -1,4 +1,6 @@
 import('jquery-ui/themes/base/all.css');
 import('./css/global_search.scss');
-import('./css/rad_common.css.scss');
+import('./css/rad_common/rad_common.css.scss');
+import('./css/bootstrap_and_overrides.scss');
+import('./css/bootstrap_datetimepicker/dates.scss')
 import('./css/deviseAuthy.scss');
