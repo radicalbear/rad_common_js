95e92768e513886a72b9fb074bacc195a626d05d
Eric Bundy <elbzero@gmail.com>
2022-10-01 12:00:04 -0400

    remove date events
    DatePicker was removed so this is not relevant anymore

--- src/areYouSure.js
+++ src/areYouSure.js
@@ -1,36 +1,7 @@
 import $ from "jquery";
 
 export class AreYouSure {
-
-
     static setup() {
         $('.simple_form').areYouSure();
-        AreYouSure.setupDatePickers();
     }
-
-    static setupDatePickers() {
-        $('.date, .time, .datetime_local').on('dp.change', function(e) {
-            let didChange = e.date && e.oldDate && !AreYouSure.sameDate(e.date, e.oldDate);
-            if(didChange) {
-                $(this).trigger('change');
-            }
-        });
-    }
-
-    static datePrecision(input) {
-        input = $(input);
-        if (input.hasClass('date')) {
-            return 'day';
-        }
-        if (input.hasClass('time')) {
-            return 'minute';
-        }
-        if (input.hasClass('datetime_local')) {
-            return 'minute';
-        }
-    };
-
-    static sameDate (date, oldDate) {
-        return date.isSame(oldDate, AreYouSure.datePrecision(this));
-    };
 }
