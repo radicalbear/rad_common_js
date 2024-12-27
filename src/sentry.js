5373fb0483cca5faeeae5734937d93647799b88b
calebtocco <32717912+calebtocco@users.noreply.github.com>
2024-12-27 06:32:00 -0500

    Try Removing replay

--- src/sentry.js
+++ src/sentry.js
@@ -2,10 +2,7 @@ import * as Sentry from '@sentry/browser';
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration()
-  ],
+  integrations: [Sentry.browserTracingIntegration()],
   tracesSampleRate: process.env.NODE_ENV == 'production' ? 0.1 : 1.0,
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0,
