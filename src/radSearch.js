a678628e42bd66a8752fc87a5ab3b0caa7301fb1
calebtocco <32717912+calebtocco@users.noreply.github.com>
2025-01-11 07:32:47 -0500

    Update radSearch.js

--- src/radSearch.js
+++ src/radSearch.js
@@ -1,21 +1,17 @@
-import $ from 'jquery';
-
 export class RadSearch {
   static setup() {
-    document.addEventListener('DOMContentLoaded', () => {
-      const saveAndApplyFiltersBtn = document.getElementById('save_and_apply_filters');
-      if (saveAndApplyFiltersBtn) {
-        saveAndApplyFiltersBtn.addEventListener('click', (e) => {
-          e.preventDefault();
-          let filterName = window.prompt('Enter a filter name', '');
-          if (filterName) {
-            document.getElementById('search_saved_name').value = filterName;
-          } else {
-            window.alert('Name was not provided, filter could not be saved.');
-          }
-          document.querySelector('.simple_form.search').submit();
-        });
-      }
-    });
+    const saveAndApplyFiltersBtn = document.getElementById('save_and_apply_filters');
+    if (saveAndApplyFiltersBtn) {
+      saveAndApplyFiltersBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        let filterName = window.prompt('Enter a filter name', '');
+        if (filterName) {
+          document.getElementById('search_saved_name').value = filterName;
+        } else {
+          window.alert('Name was not provided, filter could not be saved.');
+        }
+        document.querySelector('.simple_form.search').submit();
+      });
+    }
   }
 }
